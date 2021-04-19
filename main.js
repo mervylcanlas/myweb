742c7476c91392bba4fe53a8bc5079b6e2ea4d95
Mervyl Canlas <mervylcanlas@gmail.com>
2021-04-19 09:26:00 +0800

    test

---
+++ main.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("side_nav").style.width = "200px";
+  }
+  
+  function closeNav() {
+    document.getElementById("side_nav").style.width = "0";
+  }
